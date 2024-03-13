78e8ed9d6fdabb59c38fc635a8d345143cc51c31
jerry1704 <gerardo.garcia.q@gmail.com>
2024-03-13 17:02:56 -0600

    Fix - se cambia el gitignore

--- src/main/java/com/sistema/backconsulhbdental/Controller/InicioController.java
+++ src/main/java/com/sistema/backconsulhbdental/Controller/InicioController.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class InicioController {
+package com.sistema.backconsulhbdental.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class InicioController {
+
+    @GetMapping("/show")
+    public String Show(){
+        return "hola buen dia";
+    }
 }
