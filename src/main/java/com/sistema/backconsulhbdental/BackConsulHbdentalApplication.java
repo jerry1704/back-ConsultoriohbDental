59c9a9b49133cc64ad4bc7554920f79a528c3efa
jerry1704 <gerardo.garcia.q@gmail.com>
2024-03-13 16:30:03 -0600

    first commit

---
+++ src/main/java/com/sistema/backconsulhbdental/BackConsulHbdentalApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.backconsulhbdental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackConsulHbdentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackConsulHbdentalApplication.class, args);
+    }
+
+}
